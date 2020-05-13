fbddb60449c9aae5d7f4b7f06dca3b094ead0871
gucarletto <gucarletto@gmail.com>
2020-05-12 23:16:23 -0300

    User list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Admin } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import authProvider from './providers/authProvider';
 
-//connect the data provider to the REST endpoint
+//fake data
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 function App() {
  return (
-     <Admin dataProvider={dataProvider} />
+     <Admin dataProvider={dataProvider}>
+       <Resource name="users" list={ListGuesser}/>
+     </Admin>  
  );
 }
 
